16c56af3dbb16378872a0c6d0615c0602f0fd3c5
bbiancaa <biancagabriela.fritsch@hotmail.com>
2020-10-17 16:59:38 -0300

    iniciando o agente

--- src/br/uffs/cc/jarena/AgenteUzumaki.java
+++ src/br/uffs/cc/jarena/AgenteUzumaki.java
@@ -1,5 +1,30 @@
+//Aluna: Bianca Gabriela Fritsch
 package br.uffs.cc.jarena;
 
-public class AgenteUzumaki {
-  
+public class AgenteUzumaki extends Agente{
+  public AgenteUzumaki(Integer x, Integer y, Integer energia) {
+		super(x, y, energia);
+	}
+	
+	public void pensa() {
+		if(!podeMoverPara(getDirecao())) {
+			setDirecao(geraDirecaoAleatoria());
+		}
+	}
+	
+	public void recebeuEnergia() {
+	}
+	
+	public void tomouDano(int energiaRestanteInimigo) {
+	}
+	
+	public void ganhouCombate() {
+	}
+	
+	public void recebeuMensagem(String msg) {
+	}	
+	
+	public String getEquipe() {
+		return "Bianca";
+	}
 }
